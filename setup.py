bddbefb758106943b441eb32015e6341403dd205
Ken <kenjyco@gmail.com>
2022-04-10 10:42:25 -0500

    Bump to v0.0.4

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open('requirements.txt', 'r') as fp:
 
 setup(
     name='jira-helper',
-    version='0.0.3',
+    version='0.0.4',
     description='CLI tools and REPL for passing JQL to a JIRA server and filtering results',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/jira-helper',
-    download_url='https://github.com/kenjyco/jira-helper/tarball/v0.0.3',
+    download_url='https://github.com/kenjyco/jira-helper/tarball/v0.0.4',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
