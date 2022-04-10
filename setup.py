eafc6cc64a5d85f6161a35b4f9215d5110ee5ba6
Ken <kenjyco@gmail.com>
2022-04-10 10:42:18 -0500

    Update keywords and classifiers in setup.py

--- setup.py
+++ setup.py
@@ -32,12 +32,14 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: Libraries',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
     ],
-    keywords = ['jira', 'helper']
+    keywords=['jira', 'jql', 'repl', 'cli', 'command-line', 'helper', 'kenjyco']
 )
-
